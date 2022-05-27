08224b94d311a0330f441a925505b6a6710720a4
ChaHoHyun <ckghgusdlek@gmail.com>
2022-05-27 19:13:11 +0900

    add function for visualization

--- fuction/function.py
+++ fuction/function.py
@@ -7,12 +7,12 @@ import talib as ta
 import matplotlib.pyplot as plt
 import numpy as np
 
-api_key = '11f82ea0c8fa69832df775a90ae98bce5cf03db1'
-
 now = dt.datetime.now()
+last_year = dt.datetime.now().year - 1
 
 sktelecom = '017670.KS'
 maquerie = '088980.KS'
+ktng = '033780.KS'
 
 
 def get_stock_close(ticker, start, end):
@@ -24,15 +24,15 @@ def get_stock_close(ticker, start, end):
 def get_dividends(ticker, Dividend_count, start, end):
     stock_info = yf.Ticker(ticker)
     dividends = stock_info.dividends
+    last_dividends = dividends[-1]
     dividends_date = pd.to_datetime(dividends.index.values[-1])
     last_dividends_year = dividends_date.year
-    a = dividends_date.strftime("%Y-%m-%d")
-    if last_dividends_year == now.year:
+    if last_dividends_year >= last_year:
+        ex_dividend_date = dividends_date.strftime("%Y-%m-%d")
         print(
-            f"{ticker} \n배당락일 : {a}\n배당금 {last_dividends_year}원")
+            f"{ticker} \n배당락일 : {ex_dividend_date}\n배당금 {last_dividends}원")
     else:
-        print(f"{ticker} 올해 배당 누락")
-    last_dividends = dividends[-1]
+        print(f"Use read_spreadsheet fuction for load dividends {ticker}")
     close = get_stock_close(ticker, start, end)
     df = pd.merge(close, dividends, how='left',
                   left_index=True, right_index=True)
@@ -43,17 +43,45 @@ def get_dividends(ticker, Dividend_count, start, end):
     return df[['Adj Close', 'dividends_rate']], last_dividends
 
 
-def get_percentile(series, percentile_value):
+def get_percentile(stock_df, percentile_value):
+    series = stock_df['dividends_rate']
     series = series.dropna()
     result = round(np.percentile(series, percentile_value), 2)
     return result
 
 
+def read_spreadsheet(spreadsheet_key, worksheet_name, table_range):
+    # JSON Key File Path
+    json_key_path = '/mnt/d/my.json'
+    gc = gspread.service_account(filename=json_key_path)
+    # Open Google Spread Sheet by Key
+    doc = gc.open_by_key(spreadsheet_key)
+    sheet = doc.worksheet(worksheet_name)
+    range_list = sheet.range(table_range)
+    value_list = sheet.get(table_range)
+    df = pd.DataFrame(value_list[1:], columns=value_list[0])
+    return df
+
+
+def view_div_chart(stock_df):
+    last_divrate = stock_df['dividends_rate'].iloc[-1]
+    divrate_top10 = get_percentile(stock_df, 90)
+    plt.figure(figsize=(10, 10))
+    plt.axhline(last_divrate, 0, 1, color='blue', linestyle='--', linewidth=2)
+    plt.axhline(divrate_top10, 0, 1, color='red',
+                linestyle='--', linewidth=2)
+    stock_df['dividends_rate'].plot()
+    plt.show()
+
+
 skt_df, last_dividends = get_dividends(sktelecom, 4, 2022, now)
-# skt_divrate_top10 = get_percentile(skt_df['dividends_rate'], 90)
-# print(last_dividends, skt_divrate_top10)
-# skt_df['dividends_rate'].plot()
-# plt.show()
+skt_divrate_top10 = get_percentile(skt_df, 90)
+print(last_dividends, skt_divrate_top10)
+
 
 maquerie_df, last_dividends = get_dividends(maquerie, 2, 2018, now)
-# print(maquerie_df.head(), last_dividends)
+print(maquerie_df.head(), last_dividends)
+
+ktng_df, last_dividends = get_dividends(ktng, 1, 2008, now)
+ktng_divrate_top10 = get_percentile(ktng_df, 90)
+print(last_dividends, ktng_divrate_top10, ktng_df['dividends_rate'].iloc[-1])
--- fuction/technical_indicator.py
+++ fuction/technical_indicator.py
@@ -3,28 +3,9 @@ import talib as ta
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+spreadsheet_key = "1knsK04P6nixvkFcUpcS407HcGpMfisir6FdVwdpuUX4"
+
 
 def get_rsi(dataframe, rsi_day):
     dataframe['rsi'] = ta.RSI(dataframe["Adj Close"], rsi_day)
     return dataframe
-
-
-def read():
-    gc = gspread.service_account(
-        filename="./client_secret_887022892602-celthn1l449fsrlg7jac5pgo8md5tfs0.apps.googleusercontent.com.json")
-    sh = gc.open("dividends").workwheet("macquerie")
-    print(sh.get('E3'))
-
-
-json_key_path = '/mnt/d/my.json'  # JSON Key File Path
-
-gc = gspread.service_account(filename=json_key_path)
-
-spreadsheet_key = "spreadsheet_key"
-doc = gc.open_by_key(spreadsheet_key)
-
-sheet = doc.worksheet('macquerie')
-
-list_of_lists = sheet.get_all_values()
-value_a1 = sheet.get('E3')
-list_of_dicts = sheet.get_all_records()
